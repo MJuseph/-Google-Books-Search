66bb094b0d02643d788c2b4f43c52d229a7d9c7a
Mo-Jo <mo.juseph@gmail.com>
2019-09-12 10:14:54 -0400

    a

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 4444;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("client/build"));
+app.use(express.static(path.join(__dirname, "client/build")));
+
+
 
 app.listen(PORT, function(){
     console.log(` Global ==> API on PORT ${PORT}`);
